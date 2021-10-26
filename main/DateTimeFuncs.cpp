19bd515f4f559e6ffdb99b190cf89051d6631a50
Maksim Nezhinskii <maksimnezhin@gmail.com>
2021-10-26 16:29:26 +0300

    Add SecondsInMinute

--- main/DateTimeFuncs.cpp
+++ main/DateTimeFuncs.cpp
@@ -1,3 +1,6 @@
 bool IsLeapYear(int year) {
 	return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
 }
+int SecondsInMinute() {
+	return 60;
+}
--- main/DateTimeFuncs.h
+++ main/DateTimeFuncs.h
@@ -1,4 +1,5 @@
 #ifndef DTF_H
 #define DTF_H
 bool IsLeapYear(int year);
+int SecondsInMinute();
 #endif
