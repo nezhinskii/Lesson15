39df7372acab545b348d4d9f3a7692ea62d80766
Maksim Nezhinskii <maksimnezhin@gmail.com>
2021-10-26 16:39:17 +0300

    Add LaterInYear

--- main/DateTimeFuncs.cpp
+++ main/DateTimeFuncs.cpp
@@ -1,3 +1,11 @@
+#include<ctime>
+
 bool IsLeapYear(int year) {
 	return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+}
+
+tm& LaterInYear(tm& d1, tm& d2) {
+	if (d1.tm_mon < d2.tm_mon) return d2;
+	if (d1.tm_mon > d2.tm_mon) return d1;
+	return (d1.tm_mday < d2.tm_mday) ? d2 : d1;
 }
--- main/DateTimeFuncs.h
+++ main/DateTimeFuncs.h
@@ -1,4 +1,6 @@
 #ifndef DTF_H
 #define DTF_H
+#include <ctime>
 bool IsLeapYear(int year);
+tm& LaterInYear(const tm& d1, const tm& d2);
 #endif
